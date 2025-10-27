e48bc8960c967750b87d04226d811d07725736cd
kathanon <cower@cower.se>
2025-10-28 00:28:10 +0100

    First sketch of accessory class.

---
+++ scripts/cat/accessory.py
@@ -0,0 +1,43 @@
+from scripts.cat.save_load import load_instance
+
+class Accessory:
+    LOAD_ARGS = { 'name':    [],
+                  'color':   ['list'],
+                  'pattern': ['list'],
+                  'slot':    ['opt'],
+                  'tag':     ['opt'],
+                }
+    
+    def __init__(self, 
+                 name: str, 
+                 color: [str], 
+                 pattern: [str], 
+                 slot: str = None,
+                 tag: str = None):
+        self.name = name
+        self.color = color
+        self.pattern = pattern
+        self.slot = slot
+    
+    
+    @staticmethod
+    def load(data: dict):
+        return load_instance(data, Accessory, LOAD_ARGS)
+    
+    
+    @staticmethod
+    def load_legacy(cat_data: dict):
+        if 'accessory' not in cat_data:
+            return None
+        
+        name = cat_data['accessory']
+        color = [ cat_data['accessory_color'] ]
+        if 'accessory_color2' in cat_data:
+            color.append(cat_data['accessory_color2'])
+        pattern = [ cat_data['accessory_pattern'] ]
+        if 'accessory_pattern2' in cat_data:
+            pattern.append(cat_data['accessory_pattern2'])
+        
+        # TODO: Probably need more legacy stuff here...
+        
+        return Accessory(name, color, pattern)
--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -10,6 +10,7 @@ from scripts.cat.sprites.load_sprites import sprites
 from scripts.game_structure import constants
 from scripts.game_structure.localization import get_lang_config
 from scripts.events_module.text_adjust import adjust_list_text
+from scripts.cat.accessory import Accessory
 
 
 class Pelt:
