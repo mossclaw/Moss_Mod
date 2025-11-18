dd8371f9467070a0473bc41bfdc9c7791e42cf05
kathanon <cower@cower.se>
2025-11-18 03:12:30 +0100

    Added support for mods as zip files.

--- scripts/temp_util.py
+++ scripts/temp_util.py
@@ -1,6 +1,8 @@
 import logging
 import ujson
 import os
+import io
+from zipfile import ZipFile
 logger = logging.getLogger(__name__)
 
 
@@ -8,12 +10,11 @@ logger = logging.getLogger(__name__)
 #                            Loading json files                                #
 # ---------------------------------------------------------------------------- #
 
-def read_json(path, description_for_error = None, use_mods = True):
+def read_json(path, description_for_error = None):
     try:
         with open(path, 'r', encoding='utf-8') as read_file:
             data = ujson.loads(read_file.read())
-            if use_mods:
-                mod_expand(data, path)
+            mod_expand(data, path)
             return data
     except IOError:
         if description_for_error is not None:
@@ -39,14 +40,24 @@ def read_resource_dict(name, description_for_error = None):
 # ---------------------------------------------------------------------------- #
 
 def mod_expand(data, path):
-    global mods
-    for mod in mods:
+    global mod_mods
+    for mod in mod_mods:
         mod.expand(data, path)
 
 
 class ModMod():
-    def load(self, path):
-        return None
+    def real_path(self, path):
+        return path
+    
+    def open_path(self, path, text=True):
+        path = self.real_path(path)
+        if text:
+            return open(path, encoding='utf-8')
+        else:
+            return open(path, 'rb')
+    
+    def scan_dir(self, path):
+        return os.scandir(self.real_path(path))
     
     def expand(self, data, path):
         modded = self.load(path)
@@ -64,33 +75,79 @@ class ModMod():
             data.expand(set(modded) - set(data))
         else:
             pass # TODO: error message
+    
+    def load(self, path):
+        try:
+            with self.open_path(path) as read_file:
+                return ujson.loads(read_file.read())
+        except IOError | KeyError:
+            return None
 
 
 class DirModMod(ModMod):
     def __init__(self, path):
         self.mod_path = path
-        self.sprite_path = path + '/sprites'
-        self.sprite_config = read_dict(self.sprite_path, 'sprites', use_mods= False)
     
-    def load(self, path):
-        try:
-            with open(f"{self.mod_path}/{path}", 'r', encoding='utf-8') as read_file:
-                return ujson.loads(read_file.read())
-        except IOError:
-            return None
+    def real_path(self, path):
+        return f"{self.mod_path}/{path}"
+
+
+class ZipModMod(ModMod):
+    def __init__(self, path):
+        self.zip_path = path
+        self.zip = ZipFile(path)
+        self.fs = self.build_fs()
+    
+    def build_fs(self):
+        files = self.zip.infolist()
+        fs = {'': []}
+        for file in files:
+            parts = file.filename.split('/')
+            file.path = file.filename
+            file.name = parts[-1]
+            
+            base = prev = ''
+            for part in parts[:-1]:
+                prev = base
+                base += part + '/'
+                if base not in fs:
+                    fs[base] = []
+            fs[base if file.name else prev].append(file)
+        return fs
+
+    def open_path(self, path, text=True):
+        file = self.zip.open(path)
+        if text:
+            file = io.TextIOWrapper(file, encoding='utf-8')
+        return file
+    
+    def scan_dir(self, path):
+        if path[-1] != '/':
+            path += '/'
+        return self.fs[path] if path in self.fs else []
+
+
+class BaseMod(ModMod):
+    pass
+
 
 
 # Load mod list
-mods = []
+mod_mods = []
+base_mod = BaseMod()
 try:
     for entry in os.scandir('mods'):
         try:
+            path = entry.path
             if entry.is_dir():
-                mods.append(DirModMod(entry.path))
+                mod_mods.append(DirModMod(path))
+            elif entry.is_file() and zipfile.is_zipfile(path):
+                mod_mods.append(ZipModMod(path))
         except:
             pass
 except:
     pass
+all_mods = [base_mod] + mod_mods
 
 
 # ---------------------------------------------------------------------------- #
