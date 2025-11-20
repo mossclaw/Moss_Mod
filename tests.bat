4472da22b75694c4ae0532e40b15351d884af3a4
kathanon <cower@cower.se>
2025-11-20 22:47:14 +0100

    Minor tweak to tests bat file.

--- tests.bat
+++ tests.bat
@@ -11,13 +11,13 @@ if [%1] == [] (
 )
 
 if [%lint%] == [lint] (
+    echo.
     echo ==== Pylint ====
     uv run pylint --recursive=y --errors-only --jobs=0 E:\Programmering\Python\Moss_Mod
 )
 
-if [%1] == [] echo.
-
 if [%unit%] == [unit] (
+    echo.
     echo ==== Unit Tests ====
     uv run python -m unittest tests/test_thoughts.py tests/test_relation_events.py tests/test_group_interaction.py tests/test_conditions.py tests/test_utility.py tests/test_cat.py tests/test_save.py tests/test_filter_patrol.py tests/test_lang.py tests/test_events.py
 )
